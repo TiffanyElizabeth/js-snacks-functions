c81569217e5f775b85a8f8a10eb2b3f94b2d5eae
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-10 16:09:32 +0100

    feat: solved ex 01

--- ex-js-functions-snack-main/01 - Iniziali/script.js
+++ ex-js-functions-snack-main/01 - Iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+/**
+ * Make an array with the first letter of each word in a given list
+ * 
+ * @param {array} words - given list of words
+ * @returns {array} letters - first letter of each word in the given list
+ */
+function firstLetter(words) {
+    const letters = [];
+    for (let i = 0; i < words.length; i++) {
+        letters.push(words[i][0])
+    }
+    return letters;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(firstLetter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
