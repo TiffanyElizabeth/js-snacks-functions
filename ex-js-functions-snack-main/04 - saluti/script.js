4fdc11b17b7173ad0ea427eb304cab1de957c283
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-10 17:03:25 +0100

    feat: solved ex 04

--- ex-js-functions-snack-main/04 - saluti/script.js
+++ ex-js-functions-snack-main/04 - saluti/script.js
@@ -4,10 +4,18 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+/**
+ * 
+ * @param {string} name - the name of the user
+ * @returns {string} - a message saying "Ciao + the name of the user"
+ */
+function helloUser(name) {
+    return `Ciao ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = helloUser(userName);
+console.log(result);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
