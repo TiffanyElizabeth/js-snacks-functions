d72a56223a7264ce7eef973ce98f65d0786ab86c
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-10 16:55:26 +0100

    feat: solved ex 03

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -4,10 +4,26 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+/**
+ * 
+ * @param {string} word - word provided 
+ * @returns {number} - number of vowels found in the word provided 
+ */
 
+function numVowels(word) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let countVowels = 0;
+    for (let i = 0; i < word.length; i++) {
+        const currentLetter = word[i];
+        if (vowels.includes(currentLetter)) {
+            countVowels++;
+        }
+    }
+    return countVowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const result = numVowels(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
